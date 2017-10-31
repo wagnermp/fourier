2e36814c4f507baf6a4beb013308d688dfe4b3a6
Wagner <wmariano@id.uff.br>
2017-10-31 00:55:54 -0200

    codigo para sen 5x

--- codigo2.c
+++ codigo2.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define PI 3.141
 
+double seno(double x)
+{
+	return sin(5*x);
+}
+
+
+int main(int argc, char *argv[])
+{
+	double xi, xf, dx, x;
+	FILE *arq;
+
+	arq=fopen(argv[1],"w");
+	
+	if(!arq)
+	{
+		printf("erro");
+	}
+	
+	xi=-PI;
+	xf=PI;
+	dx=(xf-xi)/1024;
+	
+	for(x=xi; x<xf; x+=dx)
+	{
+	fprintf(arq, "%lf \t %lf \n ", x, seno(x));
+	}
+	
+}
+	
